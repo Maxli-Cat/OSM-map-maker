493d08ba07d97a36fdeb216166e62edc46c25ee8
Sophie <maxlicatenby@gmail.com>
2023-12-31 12:02:03 -0500

    Basic setup works

--- main.py
+++ main.py
@@ -2,22 +2,28 @@ import draw
 import maps
 import projections
 
-SIZE = (2500,1800)
+SIZE = (2500*2,1800*2)
+
+def buildlist(location):
+    roads = [{"points": maps.load_roads(area=location, element='"highway"="motorway"'),        "width": 5, "color": (10, 0, 0)},
+             {"points": maps.load_roads(area=location, element='"highway"="motorway_link"'),   "width": 4, "color": (10, 0, 0)},
+             {"points": maps.load_roads(area=location, element='"highway"="trunk"'),           "width": 4},
+             {"points": maps.load_roads(area=location, element='"highway"="primary"'),         "width": 3},
+             {"points": maps.load_roads(area=location, element='"highway"="secondary"'),       "width": 2},
+             {"points": maps.load_roads(area=location, element='"highway"="tertiary"'),        "width": 1},
+             {"points": maps.load_roads(area=location, element='"highway"="unclassified"'),    "width": 1, "color": (175, 175, 175)},
+             {"points": maps.load_roads(area=location, element='"highway"="residential"'),     "width": 1, "color": (125, 125, 125)},
+             {"points": maps.load_roads(area=location, element='"highway"="path"'),            "width": 1, "color": (0, 175, 0)},
+             {"points": maps.load_roads(area=location, element='"highway"="footway"'),         "width": 1, "color": (0, 175, 0)},
+             {"points": maps.load_roads(area=location, element='"highway"="track"'),           "width": 1, "color": (125, 175, 0)},
+             {"points": maps.load_roads(area=location, element='"boundary"="administrative"'), "width": 3, "color":(255,0,0)}
+             ]
+    return roads
+
 
 if __name__ == '__main__':
     img, drw = draw.setup(*SIZE)
-    roads = maps.load_roads()
-    roads += maps.load_roads(area="Rockingham, NH", element='"highway"="trunk"')
-    roads += maps.load_roads(area="Rockingham, NH", element='"highway"="primary"')
-    roads += maps.load_roads(area="Rockingham, NH", element='"highway"="secondary"')
-    roads += maps.load_roads(area="Rockingham, NH", element='"highway"="tertiary"')
-    roads += maps.load_roads(area="Rockingham, NH", element='"highway"="unclassified"')
-    roads += maps.load_roads(area="Rockingham, NH", element='"highway"="residential"')
-    roads += maps.load_roads(area="Epping, NH", element='"highway"')
-    roads += maps.load_roads(area="Freemont, NH", element='"highway"')
-    roads += maps.load_roads(area="Lee, NH", element='"highway"')
-    #roads = maps.load_roads(area="New Hampshire", element='"boundary"="administrative"')
-    map_roads = [projections.cartesian(i, bounds=SIZE) for i in roads]
-    for road in map_roads:
-        draw.drawLines(road, drw, width=1)
+    roads = buildlist("Rockingham, NH") + buildlist("Strafford, NH")
+
+    draw.drawCollection(roads, drw, projection=projections.cartesian, projection_args=[SIZE])
     img.show()
