83772af26b31ddbd3f241897c065504d2f00f2ca
Sophie <maxlicatenby@gmail.com>
2023-12-31 10:29:52 -0500

    Added drawing multiple collections with seperate settings

--- draw.py
+++ draw.py
@@ -13,3 +13,11 @@ def drawLines(points, drw : ImageDraw.ImageDraw, color=(0,0,0), width=1, closed=
     #if closed:
     #    drw.line((points[0], points[-1]), width=width, fill=color)
 
+def drawCollection(collection : dict, drw : ImageDraw.ImageDraw, default_color = (0,0,0), default_width = 1):
+    for element in collection:
+        points = collection["points"]
+        if "color" in collection.keys(): color = collection["color"]
+        else: color = default_color
+        if "width" in collection.keys(): width = collection["width"]
+        else: width = default_width
+        drawLines(points, drw, width=width, color=color)
